5a52b82fd083b1d7aad454d183adb1280a1a5835
dusanstanojeviccs <dusan.stanojevic.cs@gmail.com>
2017-06-06 12:48:13 +0200

    localhost setup

--- globals.js
+++ globals.js
@@ -5,7 +5,8 @@ module.exports = {
 	WEBSOCKET_MASTER_PORT: 8086,
 	REGULAR_SERVER_PORT: 8087,
 	STREAM_MAGIC_BYTES: 'jsmp',
-	DB_URL: 'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@ds055832.mlab.com:55832/heroku_zncrwjqm',
+	//DB_URL: 'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@ds055832.mlab.com:55832/heroku_zncrwjqm',
+	DB_URL: 'mongodb://localhost/test',
 	width: 2160,
 	height: 1350,
 }
